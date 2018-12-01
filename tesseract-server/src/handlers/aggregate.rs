3e0a4a1d495c30e0ea461089cf2af1df41d58e66
Walther Chen <walther.chen@gmail.com>
2018-11-30 23:04:48 -0500

    add querystring parsing to aggregate route

--- tesseract-server/src/handlers/aggregate.rs
+++ tesseract-server/src/handlers/aggregate.rs
@@ -1,31 +1,39 @@
 use actix_web::{
     AsyncResponder,
     FutureResponse,
+    HttpRequest,
     HttpResponse,
     Path,
-    Query,
-    State,
 };
 use clickhouse_rs::Client as ChClient;
 use futures::future::Future;
+use lazy_static::lazy_static;
 use log::*;
 use serde_derive::{Serialize, Deserialize};
+use serde_qs as qs;
 
 use crate::app::AppState;
 
 pub fn aggregate_handler(
-    (state, cube_format, _query): (State<AppState>, Path<(String, String)>, Query<AggregateQueryOpt>)
+    (req, cube_format): (HttpRequest<AppState>, Path<(String, String)>)
     ) -> FutureResponse<HttpResponse>
 {
     let (cube, format) = cube_format.into_inner();
     info!("cube: {}, format: {}", cube, format);
+
+    let query = req.query_string();
+    lazy_static!{
+        static ref QS_NON_STRICT: qs::Config = qs::Config::new(5, false);
+    }
+    let agg_query = QS_NON_STRICT.deserialize_str::<AggregateQueryOpt>(&query);
+    info!("query opts:{:?}", agg_query);
 
     // TODO put schema back in later
     let sql = format!("select * from {} limit 10", cube);
     info!("{}", sql);
 
     // TODO why have to clone?
-    ChClient::connect(state.clickhouse_options.clone())
+    ChClient::connect(req.state().clickhouse_options.clone())
         .and_then(|c| c.ping())
         .and_then(move |c| c.query_all(&sql[..]))
         .from_err()
