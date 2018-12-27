a1bfb1e70a628962040782c9ae07032d4766841c
Walther Chen <walther.chen@gmail.com>
2018-12-27 12:51:44 -0500

    [core] update growth sql test

--- tesseract-core/src/sql/growth.rs
+++ tesseract-core/src/sql/growth.rs
@@ -164,7 +164,7 @@ mod test {
 
     #[test]
     fn test_growth() {
-        let (growth, headers) = calculate("select * from test".to_owned(), "date, language, framework, ex_complete",
+        let (growth, headers) = calculate("select * from test".to_owned(), "date, language, framework, ex_complete", 1,
             &GrowthSql {
                 time_drill: DrilldownSql {
                     table: Table { name: "".to_owned(), primary_key: None, schema: None },
@@ -173,15 +173,15 @@ mod test {
                     level_columns: vec![LevelColumn { key_column: "date".to_owned(), name_column: None }],
                     property_columns: vec![],
                 },
-                mea: MeasureSql {
-                    aggregator: "".to_owned(),
-                    column: "ex_complete".to_owned(),
-                },
+                mea: "mea_1".to_owned(),
             }
         );
 
-        println!("{}", growth);
-        println!("{}", headers);
-        assert_eq!(growth, "".to_owned());
+        //println!("{}", growth);
+        //println!("{}", headers);
+        assert_eq!(
+            growth,
+            "select  language, framework, ex_complete, final_times_0, final_other_m0,  final_m, (final_m_diff / (final_m - final_m_diff)) as growth, final_m_diff\n        from (with groupArray(date) as times_0, groupArray(final_m0) as other_m0,  groupArray(mea_1) as all_m_in_group, arrayEnumerate(all_m_in_group) as all_m_in_group_ids, arrayMap( i -> i > 1 ? all_m_in_group[i] - all_m_in_group[i-1]: 0, all_m_in_group_ids) as m_diff select  language, framework, ex_complete, other_m0,  times_0, all_m_in_group, m_diff from (select * from test order by date ) group by  language, framework, ex_complete ) array Join m_diff as final_m_diff, all_m_in_group as final_m, times_0 as final_times_0 ,other_m0 as final_other_m0".to_owned(),
+            );
     }
 }
