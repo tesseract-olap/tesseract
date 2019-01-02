89a26b5e99ef9091b065d5a773605e4e5c4971ab
Walther Chen <walther.chen@gmail.com>
2019-01-02 17:59:32 -0500

    [core] optimize clickhouse rca to sum c,d from a,b; notes on rca and optimization

--- tesseract-core/src/sql/rca.rs
+++ tesseract-core/src/sql/rca.rs
@@ -1,3 +1,41 @@
+// Optimization for RCA
+// Ordinarily, just a, b, c, and d are scanned separately and then combined by joins.
+// a: (each product, each city) // can be cut on drill 1
+// b: (all products, each city)
+// c: (each product, all cities) // can be cut on drill 1
+// d: (all products, all cities)
+//
+// Note that external cuts are always valid (i.e. if above abcd were cut by a year).
+//
+// However, this results in extra scans, especially if there's no internal cuts (cuts on an rca
+// drill dim).
+//
+// The optimization is to derive the c and d aggregates from a and b. Since cuts are allowed on the
+// first drill in the rca, both a and b have to be scanned (b cannot be cut on the first drill).
+//
+// In clickhouse there is no partition, so it's trickier to do what looks like two different group
+// by.
+//
+// The general idea is to do one group by, in which both the measure and the 2nd drill are rolled
+// up.
+// - measure is rolled up by aggregate fn (e.g. sum)
+// - 2nd drill is rolled up by groupArray, which just collects all the values into an array in
+// order.
+// - the original measure is also rolled up by groupArray.
+//
+// Then the pivoted table is melted using Array Join on the 2nd drill and the original measure
+// (which would be a or c), while preserving the aggregated measure (c or d) from the pivoted
+// table.
+//
+// An example (not accounting for external cuts or dims) would be
+// select drill_1_id, drill_2_id, a, c from (
+//   select drill_1_id, groupArray(drill_2_id) as drill_2_id_s, groupArray(a) a_s, sum(a) as c from (
+//     select * from a_table
+//   )
+//   group by drill_1_id
+// )
+// array join drill_2_id_s as drill_2_id, a_s as a
+
 use itertools::join;
 
 use crate::sql::primary_agg::primary_agg;
@@ -83,50 +121,110 @@ pub fn calculate(
     println!("{:#?}", bd_cuts);
 
     // now aggregate each component
+    //
+    // As an optimization, c is calculated from a, and d is calculated from b
+    // If there's no internal cuts, then b, c, d are calculated from a.
 
+    // First do aggregation for part a, b
     let (a, a_final_drills) = primary_agg(table, &ac_cuts, &a_drills, &all_meas);
-    let (b, b_final_drills) = primary_agg(table, &bd_cuts,  &b_drills, &all_meas);
-    let (c, c_final_drills) = primary_agg(table, &ac_cuts, &c_drills, &all_meas);
-    let (d, d_final_drills) = primary_agg(table, &bd_cuts,  &d_drills, &all_meas);
-
+    let (b, b_final_drills) = primary_agg(table, &bd_cuts, &b_drills, &all_meas);
 
     // replace final_m0 with letter name.
     // I put the rca measure at the beginning of the drills, so it should
     // always be m0
     let a = a.replace("final_m0", "a");
     let b = b.replace("final_m0", "b");
-    let c = c.replace("final_m0", "c");
-    let d = d.replace("final_m0", "d");
+
+    // for clickhouse, need to make groupArray and Array Join clauses for drill_1 for when
+    // aggregating a to c, and b to d.
+    // (drill_2 would be needed if going from a to b)
+    // TODO refacto these lines out to helpers
+    let group_array_rca_drill_2 = rca.drill_2.iter()
+        .flat_map(|d| d.level_columns.iter().map(|l| {
+            if let Some(ref name_col) = l.name_column {
+                format!("groupArray({key_col}) as {key_col}_s, groupArray({name_col}) as {name_col}_s", key_col=l.key_column, name_col=name_col)
+            } else {
+                format!("groupArray({col}) as {col}_s", col=l.key_column)
+            }
+        }));
+    let group_array_rca_drill_2 = join(group_array_rca_drill_2, ", ");
+
+    let join_array_rca_drill_2 = rca.drill_2.iter()
+        .flat_map(|d| d.level_columns.iter().map(|l| {
+            if let Some(ref name_col) = l.name_column {
+                format!("{key_col}_s as {key_col}, {name_col}_s as {name_col}", key_col=l.key_column, name_col=name_col)
+            } else {
+                format!("{col}_s as {col}", col=l.key_column)
+            }
+        }));
+    let join_array_rca_drill_2 = join(join_array_rca_drill_2, ", ");
+
+    // groupArray cols (the drill_2 from rca) can't be included in the group by or select
+    let c_drills_minus_rca_drill_2 = c_drills.iter()
+        .filter(|d| !rca.drill_2.contains(&d))
+        .map(|d| d.col_string());
+    let c_drills_minus_rca_drill_2 = join(c_drills_minus_rca_drill_2, ", ");
+
+    let d_drills_minus_rca_drill_2 = d_drills.iter()
+        .filter(|d| !rca.drill_2.contains(&d))
+        .map(|d| d.col_string());
+    let d_drills_minus_rca_drill_2 = join(d_drills_minus_rca_drill_2, ", ");
+
+    // a and c drills are kept as-is
+    let a_drills_str = a_drills.iter()
+        .map(|d| d.col_string());
+    let a_drills_str = join(a_drills_str, ", ");
+
+    let b_drills_str = b_drills.iter()
+        .map(|d| d.col_string());
+    let b_drills_str = join(b_drills_str, ", ");
+
+
+    // Now add part c
+    let ac = format!("select {}, a, c from \
+                      (select {}, {}, groupArray(a) as a_s, sum(a) as c from ({}) group by {}) \
+                      Array Join {}, a_s as a",
+        a_drills_str,
+        c_drills_minus_rca_drill_2,
+        group_array_rca_drill_2,
+        a,
+        c_drills_minus_rca_drill_2,
+        join_array_rca_drill_2,
+    );
+    println!("{}", ac);
+
+    // Now add part d
+    let bd = if d_drills.is_empty() {
+            format!("select {}, b, d from \
+                        (select groupArray(b) as b_s, sum(b) as d from ({})) \
+                        Array Join {}, b_s as b",
+            b_drills_str,
+            b,
+            join_array_rca_drill_2,
+        )
+    } else {
+            format!("select {}, b, d from \
+                        (select {}, {}, groupArray(b) as b_s, sum(b) as d from ({}) group by {}) \
+                        Array Join {}, b_s as b",
+            b_drills_str,
+            d_drills_minus_rca_drill_2,
+            group_array_rca_drill_2,
+            b,
+            d_drills_minus_rca_drill_2,
+            join_array_rca_drill_2,
+        )
+    };
+
+    println!("bd: {}", bd);
 
     // now do the final join
 
     let mut final_sql = format!("select * from ({}) all inner join ({}) using {}",
-        a,
-        b,
+        ac,
+        bd,
         b_final_drills,
     );
 
-    final_sql = format!("select * from ({}) all inner join ({}) using {}",
-        c,
-        final_sql,
-        c_final_drills
-    );
-
-    // for d, cross join if d_final_drills is empty
-    final_sql = {
-        if d_final_drills.is_empty() {
-            format!("select * from ({}) cross join ({})",
-                d,
-                final_sql,
-            )
-        } else {
-            format!("select * from ({}) all inner join ({}) using {}",
-                d,
-                final_sql,
-                d_final_drills,
-            )
-        }
-    };
 
     // adding final measures at the end
     let final_ext_meas = if !meas.is_empty() {
