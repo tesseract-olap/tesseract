6540e0ffda3936aa06423bfd2d0cda0d617a6cf0
Marcio Porto <mflporto@gmail.com>
2019-01-15 11:53:01 -0500

    adds logic to deserialize JSON or XML

--- tesseract-server/src/main.rs
+++ tesseract-server/src/main.rs
@@ -56,7 +56,16 @@ fn main() -> Result<(), Error> {
     // Initialize Schema
     let schema_str = std::fs::read_to_string(&schema_path)
         .map_err(|_| format_err!("Schema file not found at {}", schema_path))?;
-    let schema = Schema::from_json(&schema_str)?;
+
+    let mut schema: Schema;
+
+    if schema_path.ends_with("xml") {
+        schema = Schema::from_xml(&schema_str)?;
+    } else if schema_path.ends_with("json") {
+        schema = Schema::from_json(&schema_str)?;
+    } else {
+        panic!("Schema format not supported");
+    }
 
     // Initialize Server
     let sys = actix::System::new("tesseract");
