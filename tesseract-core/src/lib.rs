f2c85284d4064e518a019d1b232d8296b3245a47
Jonathan Speiser <jspeis@gmail.com>
2019-06-20 12:33:50 -0400

    improves error checking

--- tesseract-core/src/lib.rs
+++ tesseract-core/src/lib.rs
@@ -207,7 +207,7 @@ impl Schema {
     ///
     /// If the function runs in negate mode, it will add or remove a ~ from the front of the default member
     /// cut string to flip the logic in order to exclude the default member from being returned in queries.
-    fn build_default_member_cuts(&self, schema_cube: &Cube, query: &Query, negate: bool) -> Vec<Cut> {
+    fn build_default_member_cuts(&self, schema_cube: &Cube, query: &Query, negate: bool) -> Result<Vec<Cut>, Error> {
         let target_dims = self.get_dims_for_default_member(schema_cube, query, negate);
         let result = target_dims.iter().map(|dim| {
             let target_hierarchy_name = match &dim.default_hierarchy {
@@ -221,18 +221,21 @@ impl Schema {
                 Some(val) => {
                     let mut new_cut_str: String = val.to_string();
                     if negate {
-                        let first_ch = new_cut_str.chars().next().expect("Expected at least one character in default member");
+                        let first_ch_opt = new_cut_str.chars().next();
+                        let first_ch = first_ch_opt.ok_or_else(|| format_err!("Expected at least one character in default member"))?;
                         new_cut_str = match first_ch {
                             '~' => new_cut_str[1..].to_string(),
                             _ => format!("~{}", new_cut_str)
                         }
-
                     }
                     Cut::from_str(&new_cut_str)
                 },
-                None => Err(format_err!("Bad default member"))
+                None => {
+                    return Err(format_err!("Bad default member"))
+                }
             }
-        }).filter_map(Result::ok).collect();
+        }).collect();
+
         return result;
     }
 
@@ -383,13 +386,13 @@ impl Schema {
 
         cut_cols.extend_from_slice(&default_hierarchy_cut_cols);
 
-        let default_member_cuts_query = self.build_default_member_cuts(schema_cube, query, false);
+        let default_member_cuts_query = self.build_default_member_cuts(schema_cube, query, false)?;
         let default_member_cut_cols = self.cube_cut_cols(&cube, &default_member_cuts_query)
             .map_err(|err| format_err!("Error creating cuts for default member: {}", err))?;
         cut_cols.extend_from_slice(&default_member_cut_cols);
 
         if query.exclude_default_members {
-            let exclude_default_member_cuts_query = self.build_default_member_cuts(schema_cube, query, true);
+            let exclude_default_member_cuts_query = self.build_default_member_cuts(schema_cube, query, true)?;
             let exclude_default_member_cut_cols = self.cube_cut_cols(&cube, &exclude_default_member_cuts_query)
                 .map_err(|err| format_err!("Error creating exclude cuts for default member: {}", err))?;
             cut_cols.extend_from_slice(&exclude_default_member_cut_cols);
