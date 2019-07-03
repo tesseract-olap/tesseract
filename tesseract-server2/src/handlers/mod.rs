03aa590162e3c2ced2bff420de49ea38907fefe5
Tali <jspeis@gmail.com>
2019-07-03 17:24:00 -0400

    includes members default handler

--- tesseract-server2/src/handlers/mod.rs
+++ tesseract-server2/src/handlers/mod.rs
@@ -17,6 +17,6 @@ mod util;
 // pub use self::flush::flush_handler;
 pub use self::index::index_handler;
 // pub use self::metadata::members_handler;
-// pub use self::metadata::members_default_handler;
+pub use self::metadata::members_default_handler;
 pub use self::metadata::metadata_handler;
 pub use self::metadata::metadata_all_handler;
