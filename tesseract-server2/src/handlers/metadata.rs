ca2ef494e13a248012f6a36fdcc8ef111fc02377
Tali <jspeis@gmail.com>
2019-07-03 17:26:47 -0400

    adds example multi-return without Box using impl Future

--- tesseract-server2/src/handlers/metadata.rs
+++ tesseract-server2/src/handlers/metadata.rs
@@ -1,11 +1,12 @@
 use actix_web::{
-    Error,
+    // Error,
     HttpRequest,
     HttpResponse,
     web::Path,
     Result as ActixResult
 };
-
+use failure::Error;
+use futures::future::Either;
 use futures::future::{self, Future};
 use lazy_static::lazy_static;
 use log::*;
@@ -37,6 +38,59 @@ pub fn metadata_all_handler(req: HttpRequest) -> HttpResponse
     HttpResponse::Ok().json(app_state.schema.read().unwrap().metadata())
 }
 
+pub fn members_default_handler(req: HttpRequest, cube: Path<String>) -> impl Future<Item=HttpResponse, Error=Error>
+{
+    let cube_format = (cube.into_inner(), "csv".to_owned());
+    do_members(req, cube_format)
+}
+
+pub fn do_members(
+    req: HttpRequest,
+    cube_format: (String, String),
+    ) -> impl Future<Item=HttpResponse, Error=Error>
+{
+    // TODO RESTORE ERROR CHECKING!
+    let (cube, format) = cube_format;
+
+    let format = format.parse::<FormatType>().unwrap();
+
+    let query = req.query_string();
+    lazy_static!{
+        static ref QS_NON_STRICT: qs::Config = qs::Config::new(5, false);
+    }
+    let query_res = QS_NON_STRICT.deserialize_str::<MembersQueryOpt>(&query).unwrap();
+
+    let level: LevelName = query_res.level.parse().unwrap();
+
+    info!("Members for cube: {}, level: {}", cube, level);
+    let app_state = req.app_data::<AppState>().unwrap();
+
+    let members_sql_and_headers = app_state.schema.read().unwrap()
+        .members_sql(&cube, &level);
+        let (members_sql, header) = match members_sql_and_headers {
+            Ok(s) => s,
+            Err(err) => {
+                return Either::A(
+                    future::result(
+                        Ok(HttpResponse::BadRequest().json(err.to_string()))
+                    )
+                );
+            },
+        };
+
+    let res = app_state
+        .backend
+        .exec_sql(members_sql)
+        .from_err()
+        .and_then(move |df| {
+            match format_records(&header, df, format) {
+                Ok(res) => Ok(HttpResponse::Ok().body(res)),
+                Err(err) => Ok(HttpResponse::NotFound().json(err.to_string())),
+            }
+        });
+
+    return Either::B(res);
+}
 
 #[derive(Debug, Deserialize)]
 struct MembersQueryOpt {
