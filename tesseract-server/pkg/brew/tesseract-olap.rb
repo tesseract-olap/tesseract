30a3709c3203db554347cfffd84b742d3917e0c0
Marcio Porto <mflporto@gmail.com>
2019-08-01 10:13:45 -0400

    updates homebrew installer with new github url

--- tesseract-server/pkg/brew/tesseract-olap.rb
+++ tesseract-server/pkg/brew/tesseract-olap.rb
@@ -1,8 +1,8 @@
 class TesseractOlap < Formula
   version '0.13.0'
   desc "ROLAP engine for web applications, in Rust."
-  homepage "https://github.com/hwchen/tesseract"
-  url "https://github.com/hwchen/tesseract/releases/download/v#{version}/tesseract-olap-#{version}-x86_64-apple-darwin.tar.gz"
+  homepage "https://github.com/tesseract-olap/tesseract"
+  url "https://github.com/tesseract-olap/tesseract/releases/download/v#{version}/tesseract-olap-#{version}-x86_64-apple-darwin.tar.gz"
   sha256 "c72b7f5f9c11d93069aac1646f84c8ba3900ac5f19a3de2277ada8543b7d112e"
 
   def install
