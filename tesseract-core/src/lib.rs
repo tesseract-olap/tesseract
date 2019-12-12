1bc31cfdee6d522a02b25bfe4de2acc4678b1ae7
Jay Majithia <jayrmajithia@gmail.com>
2019-12-12 10:00:53 -0500

    Removed the duplicate logic of the Sorting

--- tesseract-core/src/lib.rs
+++ tesseract-core/src/lib.rs
@@ -656,23 +656,17 @@ impl Schema {
             // sort column needs to be named by alias
             // Checking if we are going to sort by RCA or not
             let measure_name = s.measure.to_string();
-            if let Some(ref rca) = query.rca {
-                if measure_name == format!("{} RCA", rca.mea.to_string()){
-                    Some(SortSql {
-                        direction: s.direction.clone(),
-                        column: format!("rca"),
-                    })
-                } else {
-                    let sort_column = query.measures.iter()
-                        .position(|m| m == &s.measure)
-                        .ok_or_else(|| format_err!("sort {:?} not found in measures", &s.measure))?;
+            let rca_measure_name = if query.rca.is_some() {
+                query.rca.as_ref().unwrap().mea.to_string()
+            } else {
+                "".to_string()
+            };
 
-
-                    Some(SortSql {
-                        direction: s.direction.clone(),
-                        column: format!("final_m{}", sort_column),
-                    })
-                }
+            if measure_name == format!("{} RCA", rca_measure_name) {
+                Some(SortSql {
+                    direction: s.direction.clone(),
+                    column: format!("rca"),
+                })
             } else {
                 let sort_column = query.measures.iter()
                     .position(|m| m == &s.measure)
