aa6b0b655f3a28803cf9ee8bf0295ac8987deacf
jspeis <jspeis@gmail.com>
2019-12-17 12:26:39 -0500

    fixes for docker initialization

--- docker/scripts/init_db.sh
+++ docker/scripts/init_db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+apt-get update
+apt-get install -y git
 git clone https://github.com/tesseract-olap/tesseract-example-app.git
 cat tesseract-example-app/sql/time.sql | clickhouse-client -mn
 cat tesseract-example-app/sql/categories.sql | clickhouse-client -mn
