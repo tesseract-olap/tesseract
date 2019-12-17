1a3a96c540a05109eeaf4af8dcb0a8de6ba6c589
jspeis <jspeis@gmail.com>
2019-12-17 11:44:40 -0500

    breaks out sql commands

--- docker/scripts/init_db.sh
+++ docker/scripts/init_db.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 git clone https://github.com/tesseract-olap/tesseract-example-app.git
-cat tesseract-example-app/sql/schema.sql | clickhouse-client -mn
+cat tesseract-example-app/sql/time.sql | clickhouse-client -mn
+cat tesseract-example-app/sql/categories.sql | clickhouse-client -mn
+cat tesseract-example-app/sql/geographies.sql | clickhouse-client -mn
+cat tesseract-example-app/sql/sales.sql | clickhouse-client -mn
+
 tail -n +2 tesseract-example-app/data/time.csv | clickhouse-client --query="INSERT INTO tesseract_webshop_time FORMAT CSV";
 tail -n +2 tesseract-example-app/data/categories.csv | clickhouse-client --query="INSERT INTO tesseract_webshop_categories FORMAT CSV";
 tail -n +2 tesseract-example-app/data/geographies.csv | clickhouse-client --query="INSERT INTO tesseract_webshop_geographies FORMAT CSV";
