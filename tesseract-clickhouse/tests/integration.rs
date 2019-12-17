0270d3195d4c917e74e10b3a7babaa22960652b6
jspeis <jspeis@gmail.com>
2019-12-17 10:59:15 -0500

    adds basic integration test

--- tesseract-clickhouse/tests/integration.rs
+++ tesseract-clickhouse/tests/integration.rs
@@ -8,8 +8,10 @@ use std::{
     env
 };
 use clickhouse_rs::{
-    errors::Error, types::Block, types::Decimal, types::FromSql, ClientHandle, Pool,
+    types::Block, ClientHandle, Pool,
 };
+use clickhouse_rs::types::Complex;
+// use failure::{Error, format_err};
 
 fn database_url() -> String {
     let tmp = env::var("TESSERACT_DATABASE_URL").unwrap_or_else(|_| "tcp://localhost:9000?compression=lz4".into());
@@ -35,4 +37,33 @@ fn test_ping() {
     let pool = Pool::new(database_url());
     let done = pool.get_handle().and_then(ClientHandle::ping).map(|_| ());
     run(done).unwrap()
+}
+
+#[test]
+fn test_query() {
+    #[derive(Debug, Clone, PartialEq)]
+    pub struct RowResult {
+        pub id_count: i64,
+    }
+
+    // This test is meant as a sanity check
+    // to ensure the SQL ingestion worked
+    let pool = Pool::new(database_url());
+    let sql = "SELECT COUNT(*) as id_count FROM tesseract_webshop_time;";
+    let fut = pool.get_handle()
+        .and_then(move |c| {
+            c.query(&sql).fetch_all()
+        })
+        .and_then(move |(_, block): (_, Block<Complex>)| {
+            let schema_vec: Vec<RowResult> = block.rows().map(|row| {
+                RowResult {
+                    id_count: row.get("id_count").expect("missing id_count"),
+                }
+            }).collect();
+            Ok(schema_vec)
+        });
+
+    let res = run(fut).unwrap();
+    println!("RES={:?}", res);
+    // assert!(res.get(0).unwrap().id_count > 0);
 }
