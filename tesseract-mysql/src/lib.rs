4c5e269230c59581acdc0e15e8940a49c1c6a96a
D2SD <samueloso21@gmail.com>
2020-05-04 10:57:58 -0400

    add explicit 'dyn' to trait object

--- tesseract-mysql/src/lib.rs
+++ tesseract-mysql/src/lib.rs
@@ -26,7 +26,7 @@ impl MySql {
 }
 
 impl Backend for MySql {
-    fn exec_sql(&self, sql: String) -> Box<Future<Item=DataFrame, Error=Error>> {
+    fn exec_sql(&self, sql: String) -> Box< dyn Future<Item=DataFrame, Error=Error>> {
         let future = self.pool.get_conn()
             .and_then(move |conn| {
                 conn.prep_exec(sql.to_string(), ())
